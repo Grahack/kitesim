a47a531bbf4316ae2093992c908d4146117dc0b4
Christophe Gragnic <cgragnic@netc.fr>
2020-12-21 12:55:46 +0100

    Handle keys and draw yellow rects

--- sketch.js
+++ sketch.js
@@ -56,9 +56,36 @@ function draw() {
             pullR = y1/H;
         }
     }
-    if (keyIsDown(LEFT_ARROW))  alert("L");
-    if (keyIsDown(RIGHT_ARROW)) alert("R");
+
+    var e = 0.05;
+    if (keyIsDown(LEFT_ARROW)) {
+        if (pullL <= 1 - pullAction) {
+            pullL += pullAction;
+        }
+        if (pullL > 1-e) pullL = 1;
+    } else {
+        if (pullL >= pullRelease) {
+            pullL -= pullRelease;
+        }
+        if (pullL < e) pullL = 0;
+    }
+    if (keyIsDown(RIGHT_ARROW)) {
+        if (pullR <= 1 - pullAction) {
+            pullR += pullAction;
+        }
+        if (pullR > 1-e) pullR = 1;
+    } else {
+        if (pullR >= pullRelease) {
+            pullR -= pullRelease;
+        }
+        if (pullR < e) pullR = 0;
+    }
+    strokeWeight(0);
     fill(255, 255, 0);
-    rect(0, 0, pullWidth, (1-pullL)*H);
-    rect(W-pullWidth, 0, W, (1-pullR)*H);
+    rect(0, 0, pullWidth, pullL*groundY);
+    rect(W-pullWidth, 0, pullWidth, pullR*groundY);
+    fill(0, 0, 255);
+    rect(0, pullL*groundY, pullWidth, (1-pullL)*groundY);
+    rect(W-pullWidth, pullR*groundY, pullWidth, (1-pullR)*groundY);
+    strokeWeight(1);
 }
